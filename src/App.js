3c0c0490eb817852054c2714872a34349c47f76d
suhade <suhadetechkenya@gmail.com>
2020-04-12 06:30:35 -1200

    second commit

--- src/App.js
+++ src/App.js
@@ -11,19 +11,24 @@ function App() {
           <h3>Covid-19 Estimator</h3>
           
           <fieldset>
-            <input placeholder="Population" type="number" data="data-population" tabIndex={1} required autofocus />
+          <input placeholder="Population" type="number" data="data-population" tabIndex={1} required autofocus />
+          <label> data-population </label>
           </fieldset>
           <fieldset>
             <input placeholder="timeToElapse" type="number" data="data-time-to-elapse " tabIndex={2} required />
+            <label> data-time </label>
           </fieldset>
           <fieldset>
             <input placeholder="reportedCase" type="number" data="data-reported-cases" tabIndex={3} required />
+            <label> reportedCase </label>
           </fieldset>
           <fieldset>
             <input placeholder="totalHospitalBeds" type="number" data=" data-total-hospital-beds" tabIndex={4} required />
+            <label> HospitalBeds </label>
           </fieldset>
           <fieldset>
             <input placeholder="periodType" type="number" data="data-period-type"/>
+            <label> period </label>
           </fieldset>
           <fieldset>
             <button name="submit" type="submit" id="contact-submit" data="data-goestimate" data-submit="...Sending">Estimate.
