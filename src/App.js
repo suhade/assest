25cfa4b38b2d8202208cd054f82f6f4aeeeaa00e
suhade <suhadetechkenya@gmail.com>
2020-04-12 05:00:00 -1200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+      <div className="container">  
+        <form id="contact" action method="post">
+          <h3>Covid-19 Estimator</h3>
+          
+          <fieldset>
+            <input placeholder="Population" type="number" data="data-population" tabIndex={1} required autofocus />
+          </fieldset>
+          <fieldset>
+            <input placeholder="timeToElapse" type="number" data="data-time-to-elapse " tabIndex={2} required />
+          </fieldset>
+          <fieldset>
+            <input placeholder="reportedCase" type="number" data="data-reported-cases" tabIndex={3} required />
+          </fieldset>
+          <fieldset>
+            <input placeholder="totalHospitalBeds" type="number" data=" data-total-hospital-beds" tabIndex={4} required />
+          </fieldset>
+          <fieldset>
+            <input placeholder="periodType" type="number" data="data-period-type"/>
+          </fieldset>
+          <fieldset>
+            <button name="submit" type="submit" id="contact-submit" data="data-goestimate" data-submit="...Sending">Estimate.
+</button>
+          </fieldset>
+         
+        </form>
+      </div>
     </div>
   );
 }
