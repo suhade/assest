c924807d3c189123185c64650b8f20630f325107
suhade <suhadetechkenya@gmail.com>
2020-04-14 09:01:49 -1200

    assestment

--- src/App.js
+++ src/App.js
@@ -11,20 +11,20 @@ function App() {
           <h3>COVID-19 ESTIMATOR</h3>
           
           <fieldset>
-          <input placeholder="Population" type="number"  required autofocus tabindex="0" alt="data-population"/>
+          <input placeholder="Population" type="number"  required autofocus  alt="data-population"/>
         
           <label for="data-population">Population</label>
           </fieldset>
           <fieldset>
-            <input placeholder="Time Elapse" type="number" id ="data-time-to-elapse" required tabindex="0" alt="data-time-to-elapse"/>
+            <input placeholder="Time Elapse" type="number" id ="data-time-to-elapse" required  alt="data-time-to-elapse"/>
             <label for="data-time-to-elapse">Time Elapse</label>
           </fieldset>
           <fieldset>
-            <input placeholder="Reported Case" type="number" id ="data-reported-cases"  required tabindex="0" alt="data-reported-cases"/>
+            <input placeholder="Reported Case" type="number" id ="data-reported-cases"  required  alt="data-reported-cases"/>
             <label for="data-reported-cases">Reported Cases</label>
           </fieldset>
           <fieldset>
-            <input placeholder="Hospital Bed" type="number" id ="data-total-hospital-beds" required tabindex="0" alt="data-total-hospital-beds"/>
+            <input placeholder="Hospital Bed" type="number" id ="data-total-hospital-beds" required  alt="data-total-hospital-beds"/>
             <label for="data-total-hospital-beds">Hospital Beds</label>
           </fieldset>
           <fieldset>
