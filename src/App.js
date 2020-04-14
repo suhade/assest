1d0807cdecf4d712b1b51fa0f6aa17147137524a
suhade <suhadetechkenya@gmail.com>
2020-04-14 09:23:31 -1200

    assestment

--- src/App.js
+++ src/App.js
@@ -11,31 +11,31 @@ function App() {
           <h3>COVID-19 ESTIMATOR</h3>
           
           <fieldset>
-          <input placeholder="Population" type="number"  required autofocus  id="data-population" alt="data-population"/>
+          <input placeholder="Population" type="number"  required autofocus  id="data-population" alt="data-population" onChange={onChange}/>
         
           <label for="data-population">Population</label>
           </fieldset>
           <fieldset>
-            <input placeholder="Time Elapse" type="number" id ="data-time-to-elapse" required  alt="data-time-to-elapse"/>
+            <input placeholder="Time Elapse" type="number" id ="data-time-to-elapse" required  alt="data-time-to-elapse" onChange={onChange}/>
             <label for="data-time-to-elapse">Time Elapse</label>
           </fieldset>
           <fieldset>
-            <input placeholder="Reported Case" type="number" id ="data-reported-cases"  required  alt="data-reported-cases"/>
+            <input placeholder="Reported Case" type="number" id ="data-reported-cases"  required  alt="data-reported-cases" onChange={onChange}/>
             <label for="data-reported-cases">Reported Cases</label>
           </fieldset>
           <fieldset>
-            <input placeholder="Hospital Bed" type="number" id ="data-total-hospital-beds" required  alt="data-total-hospital-beds"/>
+            <input placeholder="Hospital Bed" type="number" id ="data-total-hospital-beds" required  alt="data-total-hospital-beds" onChange={onChange}/>
             <label for="data-total-hospital-beds">Hospital Beds</label>
           </fieldset>
           <fieldset>
-          <select placeholder="Period Type" type="number" id="data-period-type" alt="data-period-type">
+          <select placeholder="Period Type" type="number" id="data-period-type" alt="data-period-type" onChange={onChange}>
           <option value="days">Day</option>
         <option value="week">Week</option>
         <option value="month">Month</option> </select>
   <label For="data-period-type">Period</label>
          </fieldset>
           <fieldset>
-            <button name="submit" type="submit" id="contact-submit" alt="data-goestimate" data-submit="...Sending">ESTIMATE
+            <button name="submit" type="submit" id="contact-submit" alt="data-goestimate" data-submit="...Sending" onChange={onChange}>ESTIMATE
 </button>
           </fieldset>
          
